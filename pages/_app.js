82c957628708ca34d166051b6da54476cb12f4d3
sc62070 <62084630+sc62070@users.noreply.github.com>
2022-02-15 14:15:43 +0900

    app tsx > app js // npx yarn add @3rdweb/hooks

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+
+const supportedChainIds =[4]
+const connectors = {
+  injjected:{},
+}
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
